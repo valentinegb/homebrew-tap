7831c7e94d4eb62f228f6c9bbfcded4a8da3e9ed
Valentine Briese <valentinegb@icloud.com>
2023-10-23 13:31:23 -0700

    bonelab-mod-manager 0.6.3

--- Casks/bonelab-mod-manager.rb
+++ Casks/bonelab-mod-manager.rb
@@ -1,6 +1,6 @@
 cask "bonelab-mod-manager" do
-  version "0.6.2"
-  sha256 "a30d530e57a97a4e813f17102062ee6c94794b56982b44c0c0a3f5caf9665f45"
+  version "0.6.3"
+  sha256 "0b38b06c946ad753c7ba1d9c63d512df713798ed10cad7e4e3d8d67f4e2afcbb"
 
   url "https://github.com/valentinegb/bonelab_mod_manager/releases/download/v#{version}/bonelab_mod_manager.dmg"
   name "Bonelab Mod Manager"
